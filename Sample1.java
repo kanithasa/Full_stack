2de55ae83dd749138b7d047c1543426af50da9c8
kanithasa <110447425+kanithasa@users.noreply.github.com>
2024-11-20 14:14:40 +0530

    Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 public class Sample1 {
     public static void main(String args[]){
         
-        int count=2;
         int number=10;
         int prev=0;
         int next=1;
        
-        while (count<number){
+        for(int i=0;i<=number;i++){
             System.out.print(prev+" ");
             int temp=next;
             next=temp+prev;
             prev=temp;
-            count++;
+        
         }
 
         System.out.println(prev+" "+next+" ");
